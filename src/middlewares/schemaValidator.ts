70c9ccd79495914e243db3d623d0beeb6bd5c735
gleiMoura <gle.bar.mou@gmail.com>
2024-11-18 16:43:38 -0300

    ♻️ refactor: improve schemaValidator file

--- src/middlewares/schemaValidator.ts
+++ src/middlewares/schemaValidator.ts
@@ -1,11 +1,22 @@
 import { Request, Response, NextFunction } from "express";
+import chalk from "chalk";
+import { ValidationErrorItem } from "joi";
 
 export default function schemaValidator(schema: any) {
     return (req: Request, res: Response, next: NextFunction) => {
-        const validation = schema.validate(req.body, { abortEarly: false })
-        if (validation.error) {
-            return res.status(410).send(validation.error.details);
+        const { error } = schema.validate(req.body, { abortEarly: false });
+
+        if (error) {
+            console.log(chalk.red("Erro de validação:", error.details));
+
+            res.status(410).json({
+                status: 410,
+                errors: error.details.map((detail: ValidationErrorItem) => detail.message)
+            })
+
+            return
         }
+
         next();
     }
 }
